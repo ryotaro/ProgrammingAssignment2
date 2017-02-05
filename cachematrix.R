ee7a734e47e0a19281cacf1c109e240e33900f26
Ryotaro Ikeda <ryo@longsleeper.com>
2017-02-05 13:29:29 +0900

    Add cachematrix.R for programming assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R
+##
+## Compute matrix inverse with caching
 
-## Write a short comment describing this function
-
+## Creates special matrix that automatically caches computed result.
+##
+## Field variables
+##   x:     target matrix
+##   func:  a function that is used for computation
+##   cache: cache of result computed by `func(x)`
+## Member functions
+##   get:     Get the original matrix
+##   set:     Overwrite original matrix
+##   setFunc: Set a function that is used from `compute`
+##   compute: Compute `func(x)` caching the result to `cache`
 makeCacheMatrix <- function(x = matrix()) {
-
+  cache <- NULL
+  func <- NULL
+  
+  get <- function() x
+  set <- function(newMatrix) {
+    x     <<- newMatrix
+    cache <<- NULL
+  } 
+  
+  setFunc <- function(newFunc) {
+    func  <<- newFunc
+    cache <<- NULL
+  }
+  
+  compute <- function() {
+    if (is.null(func)) {
+      message('fatal: func is not set')
+      return()
+    }
+    if (!is.null(cache)) {
+      message('Using result remaining its cache')
+      return(cache)
+    }
+    
+    cache <<- func(x)
+    cache
+  }
+  
+  list(get=get, 
+       set=set, 
+       compute=compute,
+       setFunc=setFunc)
 }
 
 
-## Write a short comment describing this function
-
+## Compute inverse matrix of given matrix created by `makeCacheMatrix`
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  x$setFunc(solve)
+  x$compute()
 }
